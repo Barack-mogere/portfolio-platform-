2c70f7b31865edfbc97dc3f607654aaf011a7266
barack <barackannan00@gmail.com>
2026-08-02 16:49:22 +0300

    added a SearchBar component and implement project filtering in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
+import { useState } from "react";
 import Header from "./components/Header";
+import SearchBar from "./components/SearchBar";
 import ProjectList from "./components/ProjectList";
 
 import projects from "./data/projects";
 
 function App() {
+  const [projectList, setProjectList] = useState(projects);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const filteredProjects = projectList.filter((project) =>
+     project.title.toLowerCase().includes(searchTerm.toLowerCase()),
+   );
   return (
     <>
       <Header />
 
-      <ProjectList projects={projects} />
+      <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+
+      <ProjectList projects={filteredProjects} />
     </>
   );
 }
